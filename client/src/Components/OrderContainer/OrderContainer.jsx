6be2ba7f03b128b5f83babb2ae2ea7cebcf75a4c
Patrik_Jørgensen <vadmand806@gmail.com>
2024-09-06 10:40:06 +0200

    bestilling done

--- client/src/Components/OrderContainer/OrderContainer.jsx
+++ client/src/Components/OrderContainer/OrderContainer.jsx
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import { useSupabase } from '../../Providers/SupabaseProvider'
-import styles from './order.module.scss'
+import React, { useEffect, useState } from 'react';
+import { useSupabase } from '../../Providers/SupabaseProvider';
+import styles from './order.module.scss';
 import { OrderTopSection } from './OrderTopSection';
-import { useForm } from "react-hook-form"
+import { useForm } from 'react-hook-form';
 
 export const OrderContainer = () => {
     const [container, setContainer] = useState([]);
+    const [activeIndex, setActiveIndex] = useState(null); // State to track the active box
     const { supabase } = useSupabase();
-    const { register, handleSubmit,formState: { errors },} = useForm()
-  
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
     const getContainers = async () => {
         if (supabase) {
             const { data, error } = await supabase
-                .from("containers")
-                .select("id, name, icon_svg");
+                .from('containers')
+                .select('id, name, icon_svg');
             if (error) {
-                console.error("Error Loading Containers");
+                console.error('Error Loading Containers');
             } else {
                 setContainer(data);
             }
@@ -24,57 +25,97 @@ export const OrderContainer = () => {
 
     const onSubmit = async (data) => {
         if (supabase) {
-          try {
-            // Inserting data into the "reviews" table
-            const { data: supabaseData, error } = await supabase
-              .from("orders")
-              .insert([
-                {
-                  id: data.id,
-                  container_id: data.container_id, // Ensure `site_id` is part of your form data
-                  fullname: data.fullname, // Ensure `user_id` is part of your form data
-                  adress: data.adress, // You can generate the timestamp here
-                  zipcode: data.zipcode,
-                  city: data.city,
-                  email: data.email,
-                  phone: data.phone,
-                  created_at: data.created_at
+            try {
+                const { data: supabaseData, error } = await supabase
+                    .from('orders')
+                    .insert([
+                        {
+                            id: data.id,
+                            container_id: data.container_id,
+                            fullname: data.fullname,
+                            address: data.address,
+                            zipcode: data.zipcode,
+                            city: data.city,
+                            email: data.email,
+                            phone: data.phone,
+                            created_at: data.created_at
+                        }
+                    ]);
+
+                if (error) {
+                    console.error('Error ordering container', error);
+                } else {
+                    console.log('Message sent successfully', supabaseData);
                 }
-              ]);
-    
-            if (error) {
-              console.error("Error ordering container", error);
-            } else {
-              console.log("Message sent successfully", supabaseData);
+            } catch (error) {
+                console.error('Unexpected error', error);
             }
-          } catch (error) {
-            console.error("Unexpected error", error);
-          }
         }
-      };
-  
+    };
+
     useEffect(() => {
         getContainers();
     }, [supabase]);
-  return (
-    <div className={styles.contentWrapper}>
-        <OrderTopSection />
-    <div className={styles.orderWrapper}>
-    {container &&
-        container.map((item) => (
-            <>
-            <div  key={item.id} className={styles.containerWrapper}>
-                <section className={styles.boxWrapper}>
-                <div>
-                <p>{item.name}</p>
-                <div dangerouslySetInnerHTML={{ __html: item.icon_svg }}alt="ikke loade" />
+
+    const handleContainerClick = (index) => {
+        setActiveIndex(index);
+    };
+
+    return (
+        <div className={styles.contentWrapper}>
+            <OrderTopSection />
+            <div className={styles.orderWrapper}>
+                {container &&
+                    container.map((item, index) => (
+                        <div
+                            key={item.id}
+                            className={`${styles.containerWrapper} ${activeIndex === index ? 'active' : ''}`}
+                            onClick={() => handleContainerClick(index)}
+                        >
+                            <section className={styles.boxWrapper}>
+                                <div>
+                                    <p>{item.name}</p>
+                                    <div dangerouslySetInnerHTML={{ __html: item.icon_svg }} />
+                                </div>
+                            </section>
+                        </div>
+                    ))}
+                <div className={styles.formWrapper}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <input
+                            type="text"
+                            placeholder="Indtast dit navn"
+                            {...register('fullname', { required: true })}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Indtast din adresse"
+                            {...register('address', { required: true })}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Indtast dit postnummer"
+                            {...register('zipcode', { required: true })}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Indtast navn på din by"
+                            {...register('city', { required: true })}
+                        />
+                        <input
+                            type="email"
+                            placeholder="Indtast din email"
+                            {...register('email', { required: true })}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Indtast dit telefonnummer"
+                            {...register('phone', { required: true })}
+                        />
+                        <button type="submit">Bestil</button>
+                    </form>
                 </div>
-                </section>
             </div>
-             </>
-        ))
-    }
-    </div>
-    </div>
-  )
-}
+        </div>
+    );
+};
